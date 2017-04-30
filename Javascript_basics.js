a5524f90304e5264d0f00d2fa0af57a2d387f636
brandonjayster <jayster@protonmail.com>
2017-04-30 18:12:05 -0500

    The basics of JS

---
+++ Javascript_basics.js
@@ -0,0 +1,17 @@
+//This is a comment in javascript 
+//#######################################################################
+//#to use any of this in HTML you will add the <script>...</script> tags#
+//#																		#
+//#######################################################################
+// varaibales 
+var myName = "Brandon";
+// strings in JS 
+var string = "This is some random text";
+
+//javascripts calculator
+var addition = 5-8;// same goes for Division or any other
+// math use the console in chromes browser to test this out
+
+//Go to the next file("Java script for the web") to learn how to use this in real life Websites 
+//We will use HTML5 and CSS3 along with javascript. Do not epect jquery that comes later ;)
+//
